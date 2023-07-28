d080c45145f30e6548d3aba27a89a19fede5886e
liwenjun <ARedLion@qq.com>
2023-07-28 13:31:54 +0800

    Revert "first commit"
    This reverts commit 94a25b8facb28ca199d29202b22de13757657594.

--- env.d.ts
+++
@@ -1,9 +0,0 @@
-/// <reference types="vite/client.d.ts" />
-interface ImportMetaEnv {
-   readonly BASE_URL: string;
-   readonly VITE_BASE_URL: string;
-}
-
-interface ImportMeta {
-   readonly env: ImportMetaEnv;
-}
