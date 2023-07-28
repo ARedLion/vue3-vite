94a25b8facb28ca199d29202b22de13757657594
liwenjun <1525017188@qq.com>
2023-07-28 09:38:20 +0800

    first commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+   import TheWelcome from '../components/TheWelcome.vue'
+
+   const a = 121
+   console.log(a)
+   const x = () => {}
+   console.log(x)
+</script>
+
+<template>
+   <main>
+      <TheWelcome />
+   </main>
+</template>
